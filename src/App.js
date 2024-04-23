387afd10a9fc601c37f14e74711597d36cb09186
Marcos <marcosrocha222@gmail.com>
2024-04-23 12:55:42 -0300

    Iniciando ReactNative e Criando Backend com Firebase

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LoginScreen from './Pages/LoginScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <LoginScreen />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
